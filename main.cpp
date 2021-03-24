134247a4ee329091ad19f7e8ad0925a0541413b6
tiliang <mightbxg@hotmail.com>
2021-03-24 16:12:20 +0800

    completely same result with opencv

--- main.cpp
+++ main.cpp
@@ -55,36 +55,51 @@ int main()
     Func diffs("diffs");
     diffs(x, y, i) = cast<short>(clamped(x, y)) - cast<short>(clamped(x + ofs_buf(0, i), y + ofs_buf(1, i)));
 
+    RDom r24(0, 24, "r");
+    Func is_corner("isCorner"), count("count");
+    count(x, y) = { 0, 0 };
+    r24.where(count(x, y)[0] < 9 && count(x, y)[1] < 9);
+    Expr count_below = select(diffs(x, y, r24) < -threshold, count(x, y)[0] + 1, 0);
+    Expr count_above = select(diffs(x, y, r24) > threshold, count(x, y)[1] + 1, 0);
+    count(x, y) = { count_below, count_above };
+    is_corner(x, y) = count(x, y)[0] >= 9 || count(x, y)[1] >= 9;
+
     Func min_max_vals("min_max_vals");
     RDom ri(0, 9, "ri");
     min_max_vals(x, y, i) = { maximum(diffs(x, y, i + ri)), minimum(diffs(x, y, i + ri)) };
 
-    Func score("score");
+    Region bounds = { { 3, input.width() - 6 }, { 3, input.height() - 6 } };
+    Func score_ub("score_unbounded"), score("score");
     RDom ro(0, 16, "ro");
-    score(x, y) = max(-minimum(min_max_vals(x, y, ro)[0]), maximum(min_max_vals(x, y, ro)[1])) - 1;
+    Expr score_val = max(-minimum(min_max_vals(x, y, ro)[0]), maximum(min_max_vals(x, y, ro)[1])) - 1;
+    score_ub(x, y) = select(is_corner(x, y), score_val, 0);
+    score(x, y) = BoundaryConditions::constant_exterior(score_ub, 0, bounds)(x, y);
 
-    Func is_corner("isCorner");
-    is_corner(x, y) = score(x, y) > max(threshold - 1, score(x - 1, y), score(x + 1, y),
+    Func result_ub("result_unbounded"), result("result");
+    result_ub(x, y) = score(x, y) > max(threshold - 1, score(x - 1, y), score(x + 1, y),
                           score(x - 1, y - 1), score(x, y - 1), score(x + 1, y - 1),
                           score(x - 1, y + 1), score(x, y + 1), score(x + 1, y + 1));
+    result(x, y) = BoundaryConditions::constant_exterior(
+        result_ub, false, bounds)(x, y);
 
     // schedule
     Var yo("yo"), yi("yi");
     const int vec_size_short = get_host_target().natural_vector_size<short>();
-    is_corner.split(y, yo, yi, 32).parallel(yo).vectorize(x, vec_size_short);
-    score.compute_at(is_corner, yi).store_at(is_corner, yo).vectorize(x, vec_size_short);
+    result.split(y, yo, yi, 32).vectorize(x, vec_size_short);
+    score.compute_at(result, yi).store_at(result, yo).vectorize(x, vec_size_short);
+
+    is_corner.compute_root();
 
     // test
-    is_corner.print_loop_nest();
-    is_corner.compile_to_lowered_stmt("isCorner.html", {}, HTML);
-    Buffer<bool> output(input.width() - 8, input.height() - 8);
-    output.set_min(4, 4);
-    is_corner.realize(output);
+    //    result.print_loop_nest();
+    //    result.compile_to_lowered_stmt("isCorner.html", {}, HTML);
+    Buffer<bool> output(input.width(), input.height());
+    result.realize(output);
 
     vector<Point> result_hld;
     for (int r = 0; r < output.height(); ++r) {
         for (int c = 0; c < output.width(); ++c) {
-            int x = c + 4, y = r + 4;
+            int x = c, y = r;
             if (output(x, y))
                 result_hld.emplace_back(x, y);
         }
@@ -113,8 +128,8 @@ int main()
 #if 1
     vector<KeyPoint> result_ocv;
     // benchmark
-    double time_hld = Tools::benchmark(3, 1, [&] {
-        is_corner.realize(output);
+    double time_hld = Tools::benchmark(3, 3, [&] {
+        result.realize(output);
     });
     double time_ocv = Tools::benchmark(3, 10, [&] {
         FAST(img, result_ocv, threshold, true);
@@ -133,7 +148,7 @@ int main()
     imshow("ocv", img_ocv);
     imshow("hld", img_hld);
     imshow("diff", (img_hld - img_ocv) > 0);
-    //waitKey(0);
+    waitKey(0);
 #endif
     return 0;
 }
