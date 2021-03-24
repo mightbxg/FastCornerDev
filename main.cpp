047b28c2d58dc323257ca34834f1b8fdbf036614
tiliang <mightbxg@hotmail.com>
2021-03-24 10:54:50 +0800

    corner score

--- main.cpp
+++ main.cpp
@@ -22,11 +22,10 @@ int main()
             img.at<uchar>(pt + ofs[i]) = 50 + rand() % 10;
         }
         const int start_idx = 3;
-        for (int i = start_idx; i < start_idx + 9; ++i) {
+        for (int i = start_idx; i < start_idx + 7; ++i) {
             img.at<uchar>(pt + ofs[i]) = 150 + rand() % 10;
         }
     }
-    imshow("src", img);
 
     /******************************************************************************************/
 
@@ -47,38 +46,55 @@ int main()
     ofs_buf.set_name("ofs_buf");
     Var i("i");
 
-    Func diffs("nbs");
+    Func diffs("diffs");
     Var x("x"), y("y");
     diffs(x, y, i) = cast<short>(clamped(x, y)) - cast<short>(clamped(x + ofs_buf(0, i), y + ofs_buf(1, i)));
 
-    RDom r(0, 24, "r");
-    Func isCorner("isCorner"), count("count");
-    count(x, y) = { 0, 0 };
-    r.where(count(x, y)[0] < 9 && count(x, y)[1] < 9);
-    Expr count_below = select(diffs(x, y, r) < -6, count(x, y)[0] + 1, 0);
-    Expr count_above = select(diffs(x, y, r) > 6, count(x, y)[1] + 1, 0);
-    count(x, y) = { count_below, count_above };
-    isCorner(x, y) = count(x, y)[0] >= 9 || count(x, y)[1] >= 9;
+    RDom ri(0, 9, "ri");
+    Func max_vals("max_vals"), min_vals("min_vals");
+    max_vals(x, y, i) = maximum(diffs(x, y, i + ri));
+    min_vals(x, y, i) = minimum(diffs(x, y, i + ri));
 
-    isCorner.print_loop_nest();
-    isCorner.compile_to_lowered_stmt("isCorner.html", {}, HTML);
+    RDom ro(0, 16, "ro");
+    Func score("score");
+    score(x, y) = max(-minimum(max_vals(x, y, ro)), maximum(min_vals(x, y, ro))) - 1;
 
-    Buffer<bool> output(9, 9);
-    output.set_min(46, 46);
-    isCorner.realize(output);
-    for (int y = 0; y < 9; ++y) {
-        for (int x = 0; x < 9; ++x)
-            cout << output(x + 46, y + 46) << " ";
-        cout << endl;
-    }
-
+    // test
     Func t("t");
-    t(i) = diffs(50, 50, i);
+    t(i) = score(50, 50);
     t.trace_stores();
-    t.realize(24);
+    max_vals.compute_root();
+    min_vals.compute_root();
+    //max_vals.trace_stores();
+    min_vals.trace_stores();
+    diffs.compute_root();
+    diffs.trace_stores();
+    t.realize(1);
+
+    //    RDom r(0, 24, "r");
+    //    Func isCorner("isCorner"), count("count");
+    //    count(x, y) = { 0, 0 };
+    //    r.where(count(x, y)[0] < 9 && count(x, y)[1] < 9);
+    //    Expr count_below = select(diffs(x, y, r) < -6, count(x, y)[0] + 1, 0);
+    //    Expr count_above = select(diffs(x, y, r) > 6, count(x, y)[1] + 1, 0);
+    //    count(x, y) = { count_below, count_above };
+    //    isCorner(x, y) = count(x, y)[0] >= 9 || count(x, y)[1] >= 9;
+
+    //    isCorner.print_loop_nest();
+    //    isCorner.compile_to_lowered_stmt("isCorner.html", {}, HTML);
+
+    //    Buffer<bool> output(9, 9);
+    //    output.set_min(46, 46);
+    //    isCorner.realize(output);
+    //    for (int y = 0; y < 9; ++y) {
+    //        for (int x = 0; x < 9; ++x)
+    //            cout << output(x + 46, y + 46) << " ";
+    //        cout << endl;
+    //    }
 
     /******************************************************************************************/
 
+#if 0
     vector<KeyPoint> kps;
     FAST(img, kps, 6, true);
     cout << "kps: " << kps.size() << endl;
@@ -86,7 +102,9 @@ int main()
     cvtColor(img, img_kps, COLOR_GRAY2BGR);
     for (const auto& kp : kps)
         img_kps.at<Vec3b>(kp.pt) = { 0, 0, 255 };
+    imshow("src", img);
     imshow("kps", img_kps);
     waitKey(0);
+#endif
     return 0;
 }
